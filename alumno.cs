5ab0af123e0729bd1a21c45c4f53cb7e2f77a5d5
LENNY GUIDO BERCOVICH <49757661@ort>
2026-03-18 09:03:32 -0300

    Altos cambios

---
+++ alumno.cs
@@ -0,0 +1,19 @@
+namespace TP0._5;
+
+public class Alumno
+{
+private int dni;
+private string nombre;
+private double cantidadFaltas;
+
+public Alumno(dni, nombre, cantidadFaltas)
+{
+this.dni = dni;
+this.nombre = nombre;
+this.cantidadFaltas = cantidadFaltas;
+}
+public int getDni
+{
+    return dni;+
+}
+}
---
+++ curso.cs
@@ -0,0 +1,26 @@
+namespace TP0._5;
+
+public class Curso
+{
+
+private List<Alumno> Alumno;
+
+public Curso()
+{
+
+alumno = new List<alumno>();
+
+}
+
+public void agregarAlumno(dni, nombre, cantidadFaltas)
+{
+        Alumno a = new Alumno (dni, nombre, cantidadFaltas);       
+}
+public void buscarAlumnoxDni(){
+     bool encontrado = false
+     int i = 0;
+     while(!encontrado && i<Alumno.count){
+        alumno[i].
+     }
+}
+}
